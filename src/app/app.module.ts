eb9e67798417bdab0ebd42610e7b97e7058587bf
Aleem Latif <aleemlatif@hotmail.com>
2022-07-17 05:25:15 +1000

    -- init commit NgRx counter app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CounterComponent } from './components/counter/counter.component';
+import { counterReducer } from './reducers/counter.reducer';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    MatCardModule,
+    MatIconModule,
+    StoreModule.forRoot({ count: counterReducer }),
+    BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
